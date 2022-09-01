f67b1a709efff2da2d4c49342a14740b1639bee6
pko <pko>
2022-09-01 19:24:47 +0200

    Init commit #2

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 docker build -t qak87/qak87-ci:latest .
 
 if [ "$1" == "--publish" ]; then
-  mkdir -p $HOME/.docker/
-  echo "${DOCKER_AUTH_CONFIG}" > $HOME/.docker/config.json
+  docker login -u qak87 -p ${DOCKERHUB_PASS}
   docker push qak87/qak87-ci:latest
   docker tag qak87/qak87-ci:latest qak87/qak87-ci:"${CI_PIPELINE_IID}"
   docker push qak87/qak87-ci:"${CI_PIPELINE_IID}"
