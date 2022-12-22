d828cc1be4ed64744c01b5d68156c15f8adae2d1
przemyslawkonstanczuk <przemek.konstanczuk@gmail.com>
2022-12-22 15:58:59 +0100

    reorganizing

--- build.sh
+++
@@ -1,8 +0,0 @@
-docker build -t qak87/qak87-ci:latest .
-
-if [ "$1" == "--publish" ]; then
-  docker login -u qak87 -p ${DOCKERHUB_PASS}
-  docker push qak87/qak87-ci:latest
-  docker tag qak87/qak87-ci:latest qak87/qak87-ci:"${CI_PIPELINE_IID}"
-  docker push qak87/qak87-ci:"${CI_PIPELINE_IID}"
-fi
