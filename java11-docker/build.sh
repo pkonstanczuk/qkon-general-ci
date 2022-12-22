1450ab64c611dbeaa7c554b6755aec534ad916e5
przemyslawkonstanczuk <przemek.konstanczuk@gmail.com>
2022-12-22 16:16:03 +0100

    reorganizing

--- java11-docker/build.sh
+++ java11-docker/build.sh
@@ -1,7 +1,7 @@
 docker build -t ghcr.io/qak87/qkon-general-ci:latest .
 
 if [ "$1" == "--publish" ]; then
-  docker push ghcr.io/qak87/qkon-general-ci::latest
+  docker push ghcr.io/qak87/qkon-general-ci:latest
 #  docker tag qak87/qak87-ci:latest qak87/qak87-ci:"${CI_PIPELINE_IID}"
 #  docker push qak87/qak87-ci:"${CI_PIPELINE_IID}"
 fi
